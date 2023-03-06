518ff33e1590b7ec4f2ebdc7da98b8162612a3ca
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-03-06 11:49:55 -0300

    redux-toolkit Thunks

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,12 +1,29 @@
 
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
 
 const usersSlice = createSlice({
     name: 'users',
     initialState: {
+        isLoading: false,
+        error: null,
         data: []
     },
-    reducers: {
+    extraReducers(builder) {
+
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = true;
+        });
+
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload;
+        });
+
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        });
 
     }
 });
---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const URL = 'http://localhost:3005'
+
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+    const response =  await axios.get(URL+'/users');
+
+    return response.data;
+}); 
+
+export {fetchUsers};
