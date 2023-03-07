d9515854148bdc96e3917ff1e58b6cec01885ce4
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-03-07 09:55:54 -0300

    reusable thunk hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,25 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
-function UsersList() {
+function useThunk(thunk) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
 
-    const { isLoading, data, error } = useSelector((state) => {
+    const runThunk = useCallback(() => {
+        setIsLoading(true);
+        dispatch(
+            thunk())
+            .unwrap()
+            .catch(error => setError(error))
+            .finally(() => setIsLoading(false));
+    }, [dispatch, thunk]);
+
+    return [runThunk, isLoading, error];
+}
+
+function UsersList() {
+
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+
+    const { data } = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        dispatch(fetchUsers());
-    }, [dispatch]);
+        doFetchUsers();
+    }, [doFetchUsers]);
 
-    if (isLoading) {
-        return <Skeleton times={data} className="h-10 w-full" />
+    if (isLoadingUsers) {
+        return <Skeleton times={6} className="h-10 w-full" />
     }
 
-    if (error) {
+    if (loadingUsersError) {
         return <div>Error fetching data...</div>
     }
 
@@ -33,7 +53,7 @@ function UsersList() {
 
 
     const handleUserAdd = () => {
-        dispatch(addUser());
+        doCreateUser();
     };
 
 
@@ -41,7 +61,13 @@ function UsersList() {
         <div>
             <div className="flex flex-row justify-between m-3">
                 <h1 className="m-2 text-xl">Users</h1>
-                <Button onClick={handleUserAdd}>+ Add User</Button>
+                {
+                    isCreatingUser ? 'Creating User...' : <Button onClick={handleUserAdd}>+ Add User</Button>
+                }
+
+                {
+                    creatingUserError && 'Error Creating User...'
+                }
             </div>
             {renderedUsers}</div>
     );
